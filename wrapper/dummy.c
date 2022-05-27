e72f8e024bd4058e5d9f27aa4e81a37c0dc51a04
eecheng87 <yucheng871011@gmail.com>
2022-05-27 16:55:34 +0800

    Remove useless return value

--- wrapper/dummy.c
+++ wrapper/dummy.c
@@ -1,5 +1,5 @@
 #include "dummy.h"
 long batch_start() { return 0; }
 long batch_flush() { return 0; }
-void init_worker(int id) { return 0; }
+void init_worker(int id) { return; }
 void toggle_region() { return; }
